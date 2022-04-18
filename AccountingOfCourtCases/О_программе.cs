fcce103e14db015f582b4f6e6d50a8175f48b050
Шахзод Иномжонов <dadish0110@mail.ru>
2022-04-18 16:25:47 +0300

    Add Form, "Users, Sign in, Sign up, About"

---
+++ AccountingOfCourtCases/О_программе.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Windows.Forms;
+
+namespace AccountingOfCourtCases
+{
+    public partial class О_программе : Form
+    {
+        public О_программе()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
